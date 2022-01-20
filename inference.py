a93f57453ce2793a45fe4354a90908790bae678f
Chaskar Prasad Bhausaheb <72083319+Prasadchaskar@users.noreply.github.com>
2022-01-20 11:05:08 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -5,7 +5,7 @@ import math
 model = pickle.load(open('abalome.pkl', 'rb'))
 sex_encod = pickle.load(open('sex_lbl.pkl', 'rb'))
 def predict(df):
-    df = df[['sex', 'length', 'diameter','height', 'whole-weight', 'shucked-weight','viscera-weight','shell-weight','rings']]
+    df = df[['sex', 'length', 'diameter','height', 'whole-weight', 'shucked-weight','viscera-weight','shell-weight']]
     df.sex = sex_encod.transform(df.sex)
     predictions = model.predict(df)
     return list(np.floor(predictions))
